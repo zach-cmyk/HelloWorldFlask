30727000559363c64d87eb4134f28a737ddb7385
Zachary Nix <zacharyinix@gmail.com>
2023-04-22 05:55:03 +0100

    basic website for hello world webpage

---
+++ hello_world.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python
+from flask import Flask,render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def hello():
+    return render_template('home.html')
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0')
+
